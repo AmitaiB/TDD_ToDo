7fe293dc56946870102ec19c1756d1b77baf0c10
AmitaiB <blickstein@gmail.com>
2018-07-17 14:44:12 -0400

    Adds gitignore

---
+++ Pods/Target Support Files/Pods-ToDDo/Pods-ToDDo-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_ToDDo : NSObject
+@end
+@implementation PodsDummy_Pods_ToDDo
+@end
---
+++ Pods/Target Support Files/Pods-ToDDoTests/Pods-ToDDoTests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_ToDDoTests : NSObject
+@end
+@implementation PodsDummy_Pods_ToDDoTests
+@end
